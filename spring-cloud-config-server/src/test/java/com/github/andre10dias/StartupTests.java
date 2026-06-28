9a44ee415585cd34b50a8f655930a7eb33e25700
Carlos Dias <andre10dias@ymail.com>
2026-06-27 21:04:21 -0300

    Integrando o Spring Cloud Config com o Github

---
+++ spring-cloud-config-server/src/test/java/com/github/andre10dias/StartupTests.java
@@ -0,0 +1,13 @@
+package com.github.andre10dias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
